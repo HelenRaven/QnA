8026fad4f5a4f10ac7f223c006be0e934454a0f9
HelenRaven <helenraven@mail.ru>
2021-02-18 10:55:10 +0700

    fixed: user can delete answer

--- spec/features/answer/delete_spec.rb
+++ spec/features/answer/delete_spec.rb
@@ -6,6 +6,7 @@ feature 'Author can delete his answer', "
   I'd like to be able to delete my answer
 " do
   given(:answer) { create(:answer) }
+  given(:user) { create(:user) }
 
   scenario 'Authenticated author try to delete his answer' do
     sign_in(answer.user)
@@ -13,5 +14,18 @@ feature 'Author can delete his answer', "
     click_on 'Delete answer'
 
     expect(page).to have_content 'Your answer was successfully deleted.'
+  end
+
+  scenario 'Authenticated not author user try to delete answer' do
+    sign_in(user)
+    visit answer_path(answer)
+
+    expect(page).to_not have_content 'Delete answer'
+  end
+
+  scenario 'Unauthenticated user try to delete answer' do
+    visit answer_path(answer)
+
+    expect(page).to_not have_content 'Delete answer'
   end
 end
